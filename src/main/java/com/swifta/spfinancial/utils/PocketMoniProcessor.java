af1b17532ac19cf2d5bde49a16f16b288f82d484
oonwuzu <oonwuzu@swifta.com>
2014-11-15 01:27:35 +0100

    after implementing the PocketMoniProcessor

--- src/main/java/com/swifta/spfinancial/utils/PocketMoniProcessor.java
+++ src/main/java/com/swifta/spfinancial/utils/PocketMoniProcessor.java
@@ -1,20 +1,77 @@
 package com.swifta.spfinancial.utils;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.UnknownHostException;
 
+import com.etranzact.fundgate.ws.FundGateImplServiceStub.FundResponse;
+import com.etranzact.fundgate.ws.PocketMoneyClient;
+import com.ng.mats.psa.mt.pocketmoni.model.MoneyTransfer;
 import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.Cashinresponse;
 import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.Cashoutresponse;
 import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.ParameterExtension;
+import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.StatusCode;
 
-public class PocketMoniProcessor extends MMOProcessor{
+public class PocketMoniProcessor extends MMOProcessor {
 
 	@Override
 	public Cashoutresponse cashoutrequest(String orginatingresourceid,
 			String destinationresourceid, BigDecimal amount,
 			String sendingdescription, String receivingdescription,
 			ParameterExtension extensionparameters) {
-		// TODO Auto-generated method stub
-		return null;
+		Cashoutresponse cashoutresponse = new Cashoutresponse();
+		String userPin = "";
+		if (extensionparameters != null) {
+			userPin = extensionparameters.getExtensionparam().get(0);
+		}
+		MoneyTransfer moneyTransfer = new MoneyTransfer(orginatingresourceid,
+				destinationresourceid, null, amount.longValue(),
+				extensionparameters.getMmoperator(), sendingdescription,
+				userPin);
+
+		PocketMoneyClient pocketMoneyClient = null;
+		try {
+			pocketMoneyClient = new PocketMoneyClient();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			pocketMoneyClient.configureSecurity();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		FundResponse response = null;
+		try {
+			response = pocketMoneyClient.doCashIn(moneyTransfer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cashoutresponse.setDestinationpartnerbalanceafter(String
+				.valueOf(response.getClosingBalance()));// response.getOpeningBalance();
+
+		cashoutresponse.setOrginatingpartnerfee(null);
+		cashoutresponse.setFinancialtransactionid(response.getReference());// response.getOtherReference();
+		cashoutresponse.setOrginatingpartnerbalanceafter(String
+				.valueOf(response.getOpeningBalance()));
+		cashoutresponse.setResponseMessage(response.getMessage());
+		cashoutresponse.setStatuscode(StatusCode.COMPLETED);
+		ParameterExtension parameterExtension = new ParameterExtension();
+		parameterExtension.setSpTransactionid(response.getOtherReference());
+		parameterExtension.getExtensionparam().add(
+				String.valueOf(response.getTotalFailed()));
+		parameterExtension.getExtensionparam().add(
+				String.valueOf(response.getTotalSuccess()));
+		cashoutresponse.setExtensionparameters(parameterExtension);
+		return cashoutresponse;
+
 	}
 
 	@Override
@@ -22,10 +79,59 @@ public class PocketMoniProcessor extends MMOProcessor{
 			String destinationresourceid, BigDecimal amount,
 			String sendingdescription, String receivingdescription,
 			ParameterExtension extensionparameters) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Cashinresponse cashinresponse = new Cashinresponse();
+		String userPin = "";
+		if (extensionparameters != null) {
+			userPin = extensionparameters.getExtensionparam().get(0);
+		}
+		MoneyTransfer moneyTransfer = new MoneyTransfer(orginatingresourceid,
+				destinationresourceid, null, amount.longValue(),
+				extensionparameters.getMmoperator(), sendingdescription,
+				userPin);
 
-	
+		PocketMoneyClient pocketMoneyClient = null;
+		try {
+			pocketMoneyClient = new PocketMoneyClient();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			pocketMoneyClient.configureSecurity();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		FundResponse response = null;
+		try {
+			response = pocketMoneyClient.doCashIn(moneyTransfer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cashinresponse.setDestinationpartnerbalanceafter(String
+				.valueOf(response.getClosingBalance()));// response.getOpeningBalance();
+
+		cashinresponse.setFee(null);
+		cashinresponse.setFinancialtransactionid(response.getReference());// response.getOtherReference();
+		cashinresponse.setOrginatingpartnerbalanceafter(String.valueOf(response
+				.getOpeningBalance()));
+		cashinresponse.setResponseMessage(response.getMessage());
+		cashinresponse.setStatuscode(StatusCode.COMPLETED);
+		ParameterExtension parameterExtension = new ParameterExtension();
+		parameterExtension.setSpTransactionid(response.getOtherReference());
+		parameterExtension.getExtensionparam().add(
+				String.valueOf(response.getTotalFailed()));
+		parameterExtension.getExtensionparam().add(
+				String.valueOf(response.getTotalSuccess()));
+		cashinresponse.setExtensionparameters(parameterExtension);
+		return cashinresponse;
+
+	}
 
 }
