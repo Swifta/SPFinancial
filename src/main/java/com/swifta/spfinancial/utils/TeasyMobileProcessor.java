f50a00325aeebaf0a10a637e1d16f39c55d1fe93
oonwuzu <oonwuzu@swifta.com>
2014-11-15 16:33:40 +0100

    New Library of teasy mobile that integrates seamlessly

--- src/main/java/com/swifta/spfinancial/utils/TeasyMobileProcessor.java
+++ src/main/java/com/swifta/spfinancial/utils/TeasyMobileProcessor.java
@@ -1,6 +1,7 @@
 package com.swifta.spfinancial.utils;
 
 import java.math.BigDecimal;
+import java.util.logging.Logger;
 
 import com.mobilemoney.services.mwallet.MPWalletServiceStub.MTransferResponseType;
 import com.mobilemoney.services.mwallet.TeasyMobileClient;
@@ -11,6 +12,8 @@ import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.Param
 import com.swifta.subsidiary.mats.serviceprovider.operation.spfinancial.v1.StatusCode;
 
 public class TeasyMobileProcessor extends MMOProcessor {
+	private static final Logger logger = Logger
+			.getLogger(TeasyMobileProcessor.class.getName());
 
 	@Override
 	public Cashoutresponse cashoutrequest(String orginatingresourceid,
@@ -20,31 +23,47 @@ public class TeasyMobileProcessor extends MMOProcessor {
 		Cashoutresponse cashoutresponse = new Cashoutresponse();
 
 		MoneyTransfer teasymoneyTransfer = new MoneyTransfer(
-				destinationresourceid, amount, receivingdescription, "");
+				destinationresourceid, amount, receivingdescription,
+				extensionparameters.getExtensionparam().get(0));
 		TeasyMobileClient teasyMobileClient;
 		MTransferResponseType response = null;
 		try {
 			teasyMobileClient = new TeasyMobileClient();
-			System.out.println("posting to teasymobile");
+			logger.info("---------------------posting to teasymobile");
 			response = teasyMobileClient.doCashout(teasymoneyTransfer);
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (response.getStatus() == 0)
-			response.setResponseMessage("Transaction Successful");
-		cashoutresponse.setDestinationpartnerbalanceafter(null);
-		cashoutresponse.setOrginatingpartnerbalanceafter(null);
-		cashoutresponse.setOrginatingpartnerfee(null);
-		cashoutresponse.setStatuscode(StatusCode.COMPLETED);
-		cashoutresponse.setResponseMessage(response.getResponseMessage());
-		ParameterExtension parameterExtension = new ParameterExtension();
-		parameterExtension.setSpTransactionid(response.getTransactionId());
-		parameterExtension.getExtensionparam().add(response.getTransactionId());
-		parameterExtension.getExtensionparam().add(
-				response.getResponseMessage());
-		cashoutresponse.setExtensionparameters(parameterExtension);
+		if (response != null) {
+			if (response.getStatus() == 0) {
+				response.setResponseMessage("Transaction Successful");
+				logger.info("---------------------response returned as "
+						+ StatusCode.COMPLETED);
+				cashoutresponse.setStatuscode(StatusCode.COMPLETED);
+			} else {
+				logger.info("---------------------response returned as "
+						+ StatusCode.REJECTED);
+				cashoutresponse.setStatuscode(StatusCode.REJECTED);
+			}
+			cashoutresponse.setDestinationpartnerbalanceafter(null);
+			cashoutresponse.setOrginatingpartnerbalanceafter(null);
+			cashoutresponse.setOrginatingpartnerfee(null);
+
+			cashoutresponse.setResponseMessage(response.getResponseMessage());
+			ParameterExtension parameterExtension = new ParameterExtension();
+			parameterExtension.setSpTransactionid(response.getTransactionId());
+			parameterExtension.getExtensionparam().add(
+					response.getTransactionId());
+			parameterExtension.getExtensionparam().add(
+					response.getResponseMessage());
+			cashoutresponse.setExtensionparameters(parameterExtension);
+		} else {
+			logger.info("---------------------response returned as "
+					+ StatusCode.FAILED);
+			cashoutresponse.setStatuscode(StatusCode.FAILED);
+		}
 		return cashoutresponse;
 
 	}
@@ -69,13 +88,20 @@ public class TeasyMobileProcessor extends MMOProcessor {
 			e.printStackTrace();
 		}
 		if (response != null) {
-			if (response.getStatus() == 0)
+			if (response.getStatus() == 0) {
 				response.setResponseMessage("Transaction Successful");
+				logger.info("---------------------response returned as "
+						+ StatusCode.COMPLETED);
+				cashinresponse.setStatuscode(StatusCode.COMPLETED);
+			} else {
+				logger.info("---------------------response returned as "
+						+ StatusCode.REJECTED);
+				cashinresponse.setStatuscode(StatusCode.REJECTED);
+			}
 			cashinresponse.setDestinationpartnerbalanceafter(null);
 			cashinresponse.setFinancialtransactionid(response
 					.getTransactionId());
 			cashinresponse.setOrginatingpartnerbalanceafter(null);
-			cashinresponse.setStatuscode(StatusCode.COMPLETED);
 			cashinresponse.setFee(null);
 			cashinresponse.setResponseMessage(response.getResponseMessage());
 			ParameterExtension parameterExtension = new ParameterExtension();
@@ -86,7 +112,9 @@ public class TeasyMobileProcessor extends MMOProcessor {
 					response.getResponseMessage());
 			cashinresponse.setExtensionparameters(parameterExtension);
 		} else {
-
+			cashinresponse.setStatuscode(StatusCode.FAILED);
+			logger.info("---------------------response returned as "
+					+ StatusCode.FAILED);
 		}
 		return cashinresponse;
 	}
