ace7ab7c84d78ab6b931221d7c8abec4d875e189
oonwuzu <oonwuzu@swifta.com>
2014-11-15 04:29:21 +0100

    Finally sorted out!!!

--- src/main/java/com/swifta/subsidiary/mats/serviceprovider/operation/spfinancial/v1/ParameterExtension.java
+++ src/main/java/com/swifta/subsidiary/mats/serviceprovider/operation/spfinancial/v1/ParameterExtension.java
@@ -34,7 +34,7 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "parameterExtension", propOrder = { "mmoperator",
-		"spTransactionid", "pin", "extensionparam" })
+		"spTransactionid", "extensionparam" })
 public class ParameterExtension {
 
 	protected String mmoperator;
